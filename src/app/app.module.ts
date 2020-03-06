d86bffdd9aa838d8450b972c87fda6871632ee95
Geovanny Neves Liberato <gnliberato@outlook.com>
2020-03-05 23:49:53 -0300

    Added the project ot repository

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './star/star.component';
+import { CourseListComponent } from './courses/courses-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
